0e7a0c68d08c1ba4f1771b7fe84a6459ca86078e
Emmett <eweisz@yuhsbstudents.org>
2020-10-19 20:36:50 -0400

    Worked on DriveTrain and TeleOp
    Added methods to drive with the wheels in DriveTrain and added an empty method in TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -1,17 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class DriveTrain {
-    public DcMotor Fl,Bl,Fr,Br;
+    public DcMotor fl,bl,fr,br;
 
     public DriveTrain(){}
     public void setup(HardwareMap ahwMap){
         HardwareMap hwMap = ahwMap;
-        Fl = hwMap.dcMotor.get("Fl");
-        Bl = hwMap.dcMotor.get("Bl");
-        Fr = hwMap.dcMotor.get("Fr");
-        Br = hwMap.dcMotor.get("Br");
+        fl = hwMap.dcMotor.get("Fl");
+        bl = hwMap.dcMotor.get("Bl");
+        fr = hwMap.dcMotor.get("Fr");
+        br = hwMap.dcMotor.get("Br");
+
+        //fl.setDirection(DcMotor.Direction.FORWARD);
+        //bl.setDirection(DcMotor.Direction.FORWARD);
+        //fr.setDirection(DcMotor.Direction.FORWARD);
+        //br.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    public void rotateClockwise(double power) {
+        fl.setPower(power);
+        bl.setPower(power);
+        fr.setPower(-power);
+        br.setPower(-power);
+    }
+
+    public void strafe(double forwardLeftPower, double forwardRightPower) {
+        fl.setPower(forwardRightPower);
+        bl.setPower(forwardLeftPower);
+        fr.setPower(forwardLeftPower);
+        br.setPower(forwardRightPower);
+    }
+    public void driveForward(double power) {
+        strafe(power, power);
+    }
+    public void strafeRight(double power) {
+        strafe(-power, power);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,9 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Driver Controlled")
 public class TeleOp extends LinearOpMode {
+
+    private DriveTrain driveTrain = new DriveTrain();
+
     @Override
     public void runOpMode() {
+        setup();
+        waitForStart();
+        while(opModeIsActive()){
+
+        }
+    }
+    public void setup(){
+        driveTrain.setup(hardwareMap);
     }
 }
